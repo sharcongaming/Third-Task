59c27ed2538ce54fd5c9d6515912db2093562db4
sharcongaming <gamingsharcon@gmail.com>
2024-06-05 11:42:17 +0530

    fgdfg

--- frontent/src/Camponent/ApiConfig/index.js
+++
@@ -1,8 +0,0 @@
-// import axios from "axios";
-// const token = JSON.parse(localStorage.getItem("token"));
-
-// const api = axios.create({
-//   baseURL: "http://localhost:8000/api/v1"
-// });
-
-// export default api;
