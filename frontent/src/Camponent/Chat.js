bbec62e23a7a7bdf8e6c76dee070bb709beebfa4
sharcongaming <gamingsharcon@gmail.com>
2024-07-26 15:42:48 +0530

    done

--- frontent/src/Camponent/Chat.js
+++ frontent/src/Camponent/Chat.js
@@ -79,23 +79,47 @@ const Chat = () => {
   }, []);
   
   return (
-    <div>
+    <div className='first-div'>
       <div ref={openPopupBtnRef} id="openPopupBtn" className='chat-bot-first'>
         <i className="fa-solid fa-pen"></i>
       </div>
       <div ref={popupRef} id="popup" className="popup">
         <div className="popup-content">
-          <span ref={closePopupBtnRef} id="closePopupBtn" className="close"><i class="fa-solid fa-xmark"></i></span>
-        
-          <h2 style={{color:"black"}}>Popup Header</h2>
-          <p   style={{color:"black"}}>This is a simple popup example.</p>
-        </div>
-      
-      </div>
 
-      <div className='close-div-first'>
-            
+
+
+          <span ref={closePopupBtnRef} id="closePopupBtn" className="close"><i class="fa-solid fa-xmark"></i></span>
+          <div className='chat-first'>
+            <div className='navigaton'>
+            <div class="img_cont">
+									<img src="https://i.ibb.co/fSNP7Rz/icons8-chatgpt-512.png" class="rounded-circle user_img"/>
+									<span class="online_icon"></span>
+								</div>
+                <div class="user_info">
+									<span>ChatBot</span>
+									<p>Ask me anything!</p>
+								</div>
+
             </div>
+    <div>
+      
+    </div>
+    <div class="chat-container">
+        
+        <div class="chat-body" id="chatBody">
+        <input type="text" id="userInput" placeholder="Type your message..."/>
+        <div className='box'><i class="fas fa-location-arrow"></i></div>
+
+        </div>
+        <div class="chat-footer">
+            {/* <button onclick="sendMessage()">Send</button> */}
+        </div>
+    </div>
+
+
+    </div>
+    </div>
+    </div>
     </div>
   );
 }
